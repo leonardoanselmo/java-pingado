07d5f8d55c81592d7841b4e0ec741a60de6ab22b
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-09-27 21:39:26 -0300

    Exemplo java com Casting implicito e explicito

---
+++ Casting/src/Aplicacao.java
@@ -0,0 +1,23 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		//long x = 10L;
+		
+		//double x = 35;
+		
+		// float x = (float) 40.0;
+		
+		//short x = (short) 100000;
+		
+		byte x = (byte) 130;
+		
+		double soma = 185+130;
+		System.out.println(720-(soma+300));
+		
+		System.out.println(x);
+
+	}
+
+}
