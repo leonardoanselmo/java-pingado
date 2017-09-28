7ecd3cf2ac75d140bb95bf840b99f22a0441f6b8
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-09-27 21:42:37 -0300

    Exemplo java de uma estrutura de repetição While e FOR

---
+++ While-For/src/Aplicacao.java
@@ -0,0 +1,38 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		/*
+		int x = 0;
+		while (x <= 10){
+			x++;
+			
+			if (x == 5){
+				continue;
+			}
+			
+			System.out.println(x);
+			
+		}
+		*/
+		
+		/*
+		int x = 20;
+		do {
+			System.out.println(x);
+			x++;
+		} while (x <= 10);
+		*/
+		
+		int i;
+		int j;
+		
+		for(i = 0, j = 10; i <= 10; i++, j--){
+			System.out.println(i + " - " + j);
+		}
+		
+	}
+	
+
+}
