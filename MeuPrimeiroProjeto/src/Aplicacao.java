579a944b4ba7e92eed5a461ecffbc896d56a0142
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-09-27 21:43:43 -0300

    Exemplo java de um primeiro exemplo

---
+++ MeuPrimeiroProjeto/src/Aplicacao.java
@@ -0,0 +1,29 @@
+
+public class Aplicacao {
+
+	public static void main(String[] teste) {
+		
+		System.out.println("Meu primeiro projeto Java !!!");
+		
+		double altura = 1.8;
+		System.out.println(altura);
+		int n4 = (int) 3.5;
+		System.out.println(n4);
+		int n5 = (short) -100;
+		System.out.println(n5);
+		byte b = (byte) 129;
+		System.out.println(b);
+		
+		int idade = 15;
+		while (idade < 18) {
+			System.out.println(idade);
+			idade++;
+		}
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
