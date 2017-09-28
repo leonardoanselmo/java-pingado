17f2b44a40328d8bfcb8f7b7a0b58814ef68f757
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-09-27 21:43:11 -0300

    Exemplo java de variaveis de tipo primitivo

---
+++ Variaveis/src/Aplicacao.java
@@ -0,0 +1,34 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		//int numero;
+		//numero = 50;
+		
+		int numero = 50;
+		int numero2 = numero + 5;
+		
+		// numero = numero - 1;
+		// numero -= 1;
+		// numero--;
+		// numero *= 2;
+		
+		
+		// System.out.println(numero);
+		// System.out.println(numero2);
+		
+		//boolean b = false;		
+		//System.out.println(b);
+		
+		// long i = 10L + 5;
+		//double d = 5 * 2.0;
+		//System.out.println(i);
+		//System.out.println(d);
+		
+		double d = 10.0 / 0;
+		System.out.println(d);
+
+	}
+
+}
