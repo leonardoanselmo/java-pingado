3e8cb832abffc2afc0f322467c1ebcfc2a16b93c
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-01 11:47:33 -0300

    Exemplo java de construtores

---
+++ Construtores/src/Aplicacao.java
@@ -0,0 +1,23 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+
+		Retangulo r1 = new Retangulo();		
+		double area = r1.calcularArea();
+		
+		System.out.println("Area do retangulo: "+area);
+		
+		Retangulo r2 = new Retangulo(3, 7);
+		double area2 = r2.calcularArea();
+		
+		System.out.println("Area do retangulo 2: "+area2);
+		
+		Quadrado q = new Quadrado(2);
+		
+		System.out.println("A area do quadrado é: "+q.calcularArea());
+
+
+	}
+
+}
