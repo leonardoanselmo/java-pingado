0c7e552bc538467eb4cd08166011890fce75d3ba
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-08 19:00:53 -0300

    Alteração na classe principal

--- JanelaSwing/src/Janela.java
+++ JanelaSwing/src/Janela.java
@@ -2,15 +2,8 @@ import javax.swing.JFrame;
 
 public class Janela extends JFrame {
 	
-	public Janela() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(640, 480);
-		setVisible(true);
-		setTitle("Criando uma Janela");
-	}
-
 	public static void main(String[] args) {
-		new Janela();
+		new JanelaShow();
 	}
 
 }
