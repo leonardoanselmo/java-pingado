f2109b16056f813d0710c9ed8cbf67a0bd433899
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-08 19:27:39 -0300

    Exemplo java com Swing

---
+++ AnimaTelaDesenho/src/AnimaTelaDesenho.java
@@ -0,0 +1,42 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class AnimaTelaDesenho extends JFrame{
+
+	private JPanel tela;
+	
+	public AnimaTelaDesenho(){
+		
+		tela = new JPanel(){
+			@Override
+			protected void paintComponent(Graphics g) {
+				g.setColor(Color.BLUE);
+				g.drawLine(0, 240, 640, 240);
+				g.drawRect(10, 25, 20, 20);
+				g.drawOval(30, 20, 40, 30);
+				
+				g.setColor(Color.YELLOW);
+                g.drawLine(320, 0, 320, 480);
+                g.fillRect(110, 125, 120, 120);
+                g.fillOval(230, 220, 240, 230);
+                
+                g.setColor(Color.RED);
+                g.drawString("Eu seria um ótimo Score!", 5, 10);
+			}			
+		};
+		
+		getContentPane().add(tela);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(640, 480);
+        setVisible(true);
+        tela.repaint();	
+		
+	}
+		
+	public static void main(String[] args) {
+		new AnimaTelaDesenho();
+	}
+
+}
