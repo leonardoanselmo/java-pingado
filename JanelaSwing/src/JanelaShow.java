0159c139f4f4217c7613b88c8e7966c8fab7a4f0
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-08 19:01:25 -0300

    Adicionando uma nova classe

---
+++ JanelaSwing/src/JanelaShow.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class JanelaShow extends JFrame{
+	
+	private static final long serialVersionUID = 1L;
+
+	public JanelaShow(){
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(640, 480);
+		setVisible(true);
+		setTitle("Criando uma Janela");	
+	}
+	
+	
+
+}
