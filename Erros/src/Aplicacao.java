777a75b97e5c7ea1c2538b00a69ce9f07bc7dd2d
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-16 14:27:48 -0300

    Exemplo java explicando os Erros e tratando-os

--- Erros/src/Aplicacao.java
+++ Erros/src/Aplicacao.java
@@ -14,8 +14,44 @@ public class Aplicacao {
 		c1.getEndereco().setCidade("Ribeirão");
 		c1.getEndereco().setEstado("RJ");
 		
+		Agenda a = new Agenda();
+		a.setContato1(c1);
+		
+		imprimirNomes(a);
+		imprimirCnpj(a);
+		
 		System.out.println("Contato definido.");
 		
 	}
+	
+	private static void imprimirNomes(Agenda a){
+		if (a.getContato1() != null) {
+			System.out.println(a.getContato1().getNome());
+		}		
+		if (a.getContato2() != null && a.getContato3() != null) {
+			System.out.println(a.getContato2().getNome());
+			System.out.println(a.getContato3().getNome());	
+		}
+		
+	}
 
+	private static void imprimirCnpj(Agenda a){
+		if (a.getContato1() != null && a.getContato1() instanceof ContatoPJ)
+		{
+			ContatoPJ c = (ContatoPJ) a.getContato1();
+			System.out.println(c.getCnpj());
+		}		
+		if (a.getContato2() != null && a.getContato2() instanceof ContatoPJ){ 
+			ContatoPJ c = (ContatoPJ) a.getContato2();
+			System.out.println(c.getCnpj());
+				
+		}
+		if (a.getContato3() != null && a.getContato3() instanceof ContatoPJ) {
+			ContatoPJ c = (ContatoPJ) a.getContato3();
+			System.out.println(c.getCnpj());
+		}
+		
+	}
+
+	
 }
