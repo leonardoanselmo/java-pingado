a93f871ecf376af72bda98f670acac772bdee5a5
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-16 14:56:40 -0300

    Exemplo java com assertions

--- Assertions/src/Aplicacao.java
+++ Assertions/src/Aplicacao.java
@@ -3,6 +3,20 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		
+		ResultadoBaskara r = new ResultadoBaskara();
+				
+		//double a = 1.0;
+		//double b = 3;
+		//double c = -4;
+		
+		double a = 1;
+		double b = 2;
+		double c = 16;
+		
+		Matematica.calcularBaskara(a, b, c, r);
+		
+		System.out.println("r1 = "+r.getR1());
+		System.out.println("r2 = "+r.getR2());
 
 	}
 
