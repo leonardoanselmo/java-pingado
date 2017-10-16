77f2673f688a79501e262b0756f7c8a810836214
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-16 12:03:12 -0300

    Exemplo java com StackTrace

---
+++ StackTrace/src/PistaException.java
@@ -0,0 +1,26 @@
+
+@SuppressWarnings("serial")
+public class PistaException extends Exception{
+	
+	public PistaException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public PistaException(String message) {
+		super(message);
+	}
+
+	public PistaException(Throwable cause) {
+		super(cause);
+	}
+
+	public PistaException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public PistaException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
