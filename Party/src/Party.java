afa14ec250dd7ec2e42639caf8767777c63b9709
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-16 23:27:58 -0300

    Exemplo java com a classe awt

---
+++ Party/src/Party.java
@@ -0,0 +1,21 @@
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.Panel;
+
+public class Party {
+	
+	public static void main(String[] args) {
+	
+		Frame f = new Frame();
+		Label l = new Label("Party as Tim's");
+		
+		Button b = new Button("You bet");
+		Button c = new Button("Show me");
+		
+		Panel p = new Panel();
+		p.add(l);
+		
+	}	
+	
+}
