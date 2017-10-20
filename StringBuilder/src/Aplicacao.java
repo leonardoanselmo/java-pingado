391c680b78e995d693d16c8d021c8ced8b06906f
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-19 22:28:10 -0300

    Exemplo Java com StringBuilder

---
+++ StringBuilder/src/Aplicacao.java
@@ -0,0 +1,15 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		String s = "";
+		
+		for (int i = 0; i < 1000; i++) {
+			s = s + "X";
+		}
+
+		System.out.println(s);
+	}
+
+}
