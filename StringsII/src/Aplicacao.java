07b1554d0ad87ad0e7e2e74ab969114fcc036262
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-19 22:20:20 -0300

    Exemplo Java com Strings

--- StringsII/src/Aplicacao.java
+++ StringsII/src/Aplicacao.java
@@ -5,12 +5,19 @@ public class Aplicacao {
 		
 		String s1 = new String("abc");
 		String s2 = "abc";
+		String s3 = "abc";
 		System.out.println(s1);
 		System.out.println(s2);
 		
 		System.out.println(s1 == s2);
 		System.out.println(s1.equals(s2));
+		
+		s2 = s2.toUpperCase();
+		System.out.println(s2);
 
+		s2 = s2.concat(s3);
+		System.out.println(s2);
+		
 	}
 
 }
