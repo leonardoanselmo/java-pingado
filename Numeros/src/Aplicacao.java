3348f5b347c4826f7658a85ede9d97f5465f31e2
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-19 22:50:58 -0300

    Exemplo Java com formatação de números

--- Numeros/src/Aplicacao.java
+++ Numeros/src/Aplicacao.java
@@ -1,14 +1,36 @@
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
+import java.util.Locale;
 
 public class Aplicacao {
 
 	public static void main(String[] args) {
 		
-		double d = 145.4;
+		double d = 1145.4;
 		
 		NumberFormat nf1 = NumberFormat.getNumberInstance();
 		String s1 = nf1.format(d);
 		System.out.println(s1);
+		
+		NumberFormat nf2 = NumberFormat.getNumberInstance(new Locale("en", "US"));
+		String s2 = nf2.format(d);
+		System.out.println(s2);
+		
+		NumberFormat nf3 = NumberFormat.getCurrencyInstance();
+		String s3 = nf3.format(d);
+		System.out.println(s3);
+		
+		NumberFormat nf4 = NumberFormat.getCurrencyInstance(new Locale("en", "US"));
+		String s4 = nf4.format(d);
+		System.out.println(s4);
+		
+		DecimalFormat df1 = new DecimalFormat("00000.000");
+		DecimalFormatSymbols dfs = new DecimalFormatSymbols();
+		dfs.setDecimalSeparator('-');
+		df1.setDecimalFormatSymbols(dfs);
+		String s5 = df1.format(d);
+		System.out.println(s5);
 
 	}
 
