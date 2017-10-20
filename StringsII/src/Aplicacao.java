c78363e2dcdac3c7fced77c32d0afb9f614bc575
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-19 22:05:21 -0300

    Exemplo Java com Strings

---
+++ StringsII/src/Aplicacao.java
@@ -0,0 +1,16 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		String s1 = new String("abc");
+		String s2 = "abc";
+		System.out.println(s1);
+		System.out.println(s2);
+		
+		System.out.println(s1 == s2);
+		System.out.println(s1.equals(s2));
+
+	}
+
+}
