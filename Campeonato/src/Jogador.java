8a073d5c70d456d5e246decac6ebb1e83f444dc2
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-20 22:53:54 -0300

    Exemplo java com aplicacao de um Campeonato de Futebol

---
+++ Campeonato/src/Jogador.java
@@ -0,0 +1,32 @@
+
+public class Jogador {
+	
+	private String nome;
+	private Time time;
+	private String posicao;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Time getTime() {		
+		return time;
+	}
+	
+	public void setTime(Time time) {
+		this.time = time;
+	}
+	
+	public String getPosicao() {
+		return posicao;
+	}
+	
+	public void setPosicao(String posicao) {
+		this.posicao = posicao;
+	}
+
+}
