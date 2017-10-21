b83b26ae0ee3fa6bee6231b9543177fa2043e418
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-21 19:20:06 -0300

    Exemplo java com Datas

--- DatasII/src/Aplicacao.java
+++ DatasII/src/Aplicacao.java
@@ -1,3 +1,4 @@
+import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -14,10 +15,24 @@ public class Aplicacao {
 		cal.set(Calendar.MONTH, 1);
 		
 		cal.set(2020, 1, 5, 13, 30, 0);
+		cal.add(Calendar.YEAR, 1);
+		cal.add(Calendar.MINUTE, -30);
 		
 		Date d = cal.getTime();
 		System.out.println(d);
-
+		
+		DateFormat df1 = DateFormat.getDateInstance(DateFormat.SHORT);
+		String f1 = df1.format(d);
+		System.out.println(f1); // Século com 2 dígitos.
+		
+		DateFormat df2 = DateFormat.getDateInstance(DateFormat.MEDIUM);
+		String f2 = df2.format(d);
+		System.out.println(f2); // Século com 4 dígitos.
+		
+		DateFormat df3 = DateFormat.getDateInstance(DateFormat.LONG);
+		String f3 = df3.format(d);
+		System.out.println(f3);
+		
 	}
 
 }
