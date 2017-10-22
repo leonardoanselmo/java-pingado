5f45d20a883871ce64988012abab4b5188648711
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-22 20:25:13 -0300

    Exemplo java com numeros randômicos criando uma semente

--- Random/src/Aplicacao.java
+++ Random/src/Aplicacao.java
@@ -4,13 +4,20 @@ public class Aplicacao {
 		
 	public static void main(String[] args) {
 		
-		Random r = new Random();
+		Random r = new Random(12345678);
 		
 		for(int i =0; i < 10; i++){
-			int d = r.nextInt(100);
+			// int d = r.nextInt(100);
+			int d = gerarInt(50, 89);
 			System.out.println((i+1) + " => " + d);
 		}
 
+	}
+	
+	public static int gerarInt(int inicio, int fim){
+		int intervalo = (fim - inicio);
+		int n = (int) (Math.random() * intervalo) + inicio;
+		return n;
 	}
 
 }
