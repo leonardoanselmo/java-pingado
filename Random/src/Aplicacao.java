9cc77a847c10386c9ee12f33975398274626a240
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-21 21:28:18 -0300

    Exemplo java com numeros randômicos

--- Random/src/Aplicacao.java
+++ Random/src/Aplicacao.java
@@ -1,10 +1,13 @@
+import java.util.Random;
 
 public class Aplicacao {
 
 	public static void main(String[] args) {
 		
+		Random r = new Random();
+		
 		for(int i =0; i < 10; i++){
-			double d = Math.random();
+			int d = r.nextInt(100);
 			System.out.println((i+1) + " => " + d);
 		}
 
