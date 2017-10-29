3d8dc70d0b85c23b33a38558a2593edf38e9046c
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-28 21:25:28 -0300

    Exemplo java com enums

---
+++ Enums/src/Aplicacao.java
@@ -0,0 +1,27 @@
+
+public class Aplicacao {
+	
+	private enum Sexo {
+		MASCULINO, FEMININO;
+	}
+	
+	public static void main(String[] args){
+		
+		DiaDaSemana hoje = DiaDaSemana.SEXTA;
+		System.out.println(hoje);
+		
+		int num = DiaDaSemana.QUARTA.getNum();
+		System.out.println(num);
+
+		String s = "TERCA";
+		
+		DiaDaSemana d = DiaDaSemana.valueOf(s);
+		System.out.println("O dia é: " + d + " seu numero é: "+d.getNum());
+		
+		Sexo sexo = Sexo.MASCULINO;
+		System.out.println(sexo);
+		
+		
+	}
+
+}
