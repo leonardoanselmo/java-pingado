524d0b825b82c44334ba45a4cf3cb629e0b88b9b
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-13 19:46:05 -0300

    Exemplo Java para somar 2 números

---
+++ Somando/src/Aplicacao.java
@@ -0,0 +1,14 @@
+
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		DoisNumeros doisnumeros = new DoisNumeros();
+		
+		double result = doisnumeros.somar(2,2);
+		System.out.println(result);
+
+	}
+
+}
