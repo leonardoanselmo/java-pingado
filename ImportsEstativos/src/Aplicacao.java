ea09ca58e89bff24bf0e7755555aa77abe260e64
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-14 08:56:16 -0300

    Exemplo Java com Imports Estaticos

---
+++ ImportsEstativos/src/Aplicacao.java
@@ -0,0 +1,13 @@
+import static java.lang.Math.round;
+
+
+public class Aplicacao {
+	
+	public static void main(String[] args) {
+		
+		double numero = round(11.5);
+		System.out.println(numero);
+	
+	}
+
+}
