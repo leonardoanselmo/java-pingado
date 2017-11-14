866a3721375b3913713d74be181bca9694e2060e
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-14 10:46:08 -0300

    Exemplo java com o uso de um jar/Util criado como uma lib

---
+++ Projeto/src/Aplicacao.java
@@ -0,0 +1,12 @@
+import plugueinformatica.util.StringUtils;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		boolean string = StringUtils.isEmpty("abc   ");
+		System.out.println(string);
+
+	}
+
+}
