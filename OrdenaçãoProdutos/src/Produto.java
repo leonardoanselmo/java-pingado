3665f9261427cd828d92f15b2299e5f4fb2d3dce
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-18 18:45:29 -0300

    Classe Produto

--- OrdenaçãoProdutos/src/Produto.java
+++ OrdenaçãoProdutos/src/Produto.java
@@ -17,4 +17,34 @@ public class Produto {
 		return valor;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Produto: "+nome+" - Valor: "+valor;
+	}
+
 }
