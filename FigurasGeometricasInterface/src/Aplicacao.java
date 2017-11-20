9ea39d602416a794483e6859abd273610cf20438
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-20 19:54:12 -0300

    Classe principal

--- FigurasGeometricasInterface/src/Aplicacao.java
+++ FigurasGeometricasInterface/src/Aplicacao.java
@@ -3,6 +3,33 @@ public class Aplicacao {
 	
 	public static void main(String[] args) {
 		
+		Quadrado[] objQuadrado = new Quadrado[4];
+
+        int x = 0;
+        while (x < objQuadrado.length){
+            objQuadrado[x] = new Quadrado(x+2);
+            double result = objQuadrado[x].calcularArea();
+            System.out.println("A área do Quadrado: "+x+" é: "+result);
+            x++;
+        }
+
+        System.out.println();
+
+        for (int i = 0; i<objQuadrado.length;i++){
+            objQuadrado[i] = new Quadrado(i+2);
+            System.out.println("A área do Quadrado: "+i+" é: "+objQuadrado[i].calcularArea());
+        }
+
+        System.out.println();
+
+        Circulo[] objCirculo = new Circulo[3];
+        
+        for (int i = 0; i < objCirculo.length; i++){
+            objCirculo[i] = new Circulo(3);
+            System.out.println("A area do Círculo: "+i+" é: "+
+                    String.format("%.2f", objCirculo[i].calcularArea()));
+        }
+        
 	}
 
 }
