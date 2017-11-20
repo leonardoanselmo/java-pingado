3dd368c5c56fc2f2ec0b8164ed2834c3e035f8d4
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-20 19:50:28 -0300

    Classe Circulo

---
+++ FigurasGeometricasInterface/src/Circulo.java
@@ -0,0 +1,15 @@
+
+public class Circulo implements Figura {
+	
+	private double raio;
+	
+	public Circulo(double raio){
+		this.raio = raio;
+	}	
+
+	@Override
+	public double calcularArea() {		
+		return (Math.PI * (raio * raio));
+	}
+
+}
