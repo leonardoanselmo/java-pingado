3e5e8ffa02398f8bd4ef6dff39324e5f76694c6b
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-22 12:50:08 -0300

    Classe FactoryPessoa

---
+++ PadraoFactory/src/FactoryPessoa.java
@@ -0,0 +1,14 @@
+
+public class FactoryPessoa {
+	
+	public Pessoa getPessoa(String nome, String sexo){
+		
+		if (sexo.equals("M")) {
+			return new Homem(nome);
+		} else {
+			return new Mulher(nome);
+		}
+		
+	}
+
+}
